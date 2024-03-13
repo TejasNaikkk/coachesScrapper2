1a5111494151ae8664890ef331e2b97063cdeb81
Tejas Naik <tejasnaik121@gmail.com>
2024-03-13 13:52:28 +0530

    Final main of Data Extraction Phase

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
 import config
 from souper import souper
 from getDetails import getInfo
+import pymongo
 
 url = config.url
 testURL = config.testURL
+conn = config.mongoLocal
 
-
+# Main soup
 soup = souper(url)
 h3s = soup.find_all('h3', class_='tb-heading')
 
+# Create connection with mongo
+client = pymongo.MongoClient(conn)
+db = client["Scrapped"]
+collection = db["Coaches2"]
+
+def insertDataToMongo(payload, collection):
+    collection.insert_one(payload)
 
 users = []
-
 for h3 in h3s:
     a = h3.find('a')
     href = a['href']
     users.append(href)
 
-test = getInfo(testURL)
-print(test)
+for user in users:
+    payload = getInfo(user)
+    insertDataToMongo(payload,collection)
+
+client.close()
